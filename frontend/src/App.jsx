4c845a376b6803033ff8d1ddfdb150a14ccc43e4
SaurabhLucifer <saurabhsinghh2r@gmail.com>
2024-04-27 23:29:36 +0530

    UI Design completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+
+import Sidebar from "./components/Sidebar";
+//import { useAuthContext } from "./context/AuthContext";
+function App() {
+  return <div className="flex ">
+<Sidebar />
+<div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/signup" element={<SignupPage />} />
+    <Route path="/explore" element={<ExplorePage />} />
+    <Route path="/likes" element={<LikesPage />} />
+
+  </Routes>
+  <footer>footer</footer>
+</div>
+
+  </div>
+}
+
+export default App
